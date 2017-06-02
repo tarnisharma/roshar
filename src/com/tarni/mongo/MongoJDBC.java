b1fafad5392650ab3eb42a187fa2781fb06b6ff9
Tarni Sharma <tarni.sharma@gmail.com>
2017-06-03 00:19:54 +0530

    Added more functions

--- src/com/tarni/mongo/MongoJDBC.java
+++ src/com/tarni/mongo/MongoJDBC.java
@@ -4,7 +4,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.WriteResult;
 
 public class MongoJDBC {
 
@@ -33,31 +35,68 @@ public class MongoJDBC {
 	}
 
 	public void addDocumentToCollection(DBCollection coll2) {
-		BasicDBObject doc = new BasicDBObject("title", "MongoDB").append("description", "database").append("likes", 100)
-				.append("url", "http://www.tutorialspoint.com/mongodb/").append("by", "tutorials point");
+		DBObject doc = new BasicDBObject("title", "MongoDBNew").append("description", "database").append("likes", 10)
+				.append("url", "http://www.tutorialspoint.com/mongodb/").append("by", "Tarni");
 
 		coll2.insert(doc);
 		System.out.println("Document inserted successfully");
 	}
 
-	public void showDocuments(DBCollection coll){
+	public void showDocuments(DBCollection coll) {
 		DBCursor cursor = coll.find();
-        int i = 1;
-			
-        while (cursor.hasNext()) { 
-           System.out.println("Inserted Document: "+i); 
-           System.out.println(cursor.next()); 
-           i++;
-        }
+		int i = 1;
+
+		while (cursor.hasNext()) {
+			System.out.println("Inserted Document: " + i);
+			System.out.println(cursor.next());
+			i++;
+		}
 	}
-	
+
+	public void find(DBCollection coll) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("title", "MongoDB");
+		DBCursor cursor = coll.find(query);
+		int i = 1;
+		while (cursor.hasNext()) {
+			System.out.println("Document found: " + i);
+			System.out.println(cursor.next());
+			i++;
+		}
+	}
+
+	public void delete(DBCollection coll) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("title", "MongoDBTarni");
+		WriteResult result = coll.remove(query);
+		System.out.println("Deletion Result "+result.toString());
+		DBCursor cursor = coll.find();
+		int i = 1;
+		while (cursor.hasNext()) {
+			System.out.println("Document left: " + i);
+			System.out.println(cursor.next());
+			i++;
+		}
+	}
+
+	public void updateDocument(DBCollection coll) {
+		BasicDBObject query = new BasicDBObject();
+		query.put("title", "MongoDB");
+		BasicDBObject newDocument = new BasicDBObject();
+		newDocument.put("title", "MongoDBTarni");
+		BasicDBObject updateObj = new BasicDBObject();
+		updateObj.put("$set", newDocument);
+		WriteResult result = coll.update(query, updateObj);
+		System.out.println("Update Result "+result.toString());
+	}
+
 	public static void main(String args[]) {
 		MongoJDBC mongoJDBCConnector = new MongoJDBC();
 		mongoJDBCConnector.connectToDB();
 		DBCollection coll = mongoJDBCConnector.createCollection();
 		mongoJDBCConnector.addDocumentToCollection(coll);
-		mongoJDBCConnector.addDocumentToCollection(coll);
-		mongoJDBCConnector.addDocumentToCollection(coll);
-		mongoJDBCConnector.showDocuments(coll);
+		mongoJDBCConnector.find(coll);
+		mongoJDBCConnector.updateDocument(coll);
+		mongoJDBCConnector.delete(coll);
 	}
 }
